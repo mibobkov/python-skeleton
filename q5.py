6c0c49925901373d019f3770d7174cc0561f12c5
mibobkov <mibobkov@gmail.com>
2018-10-22 19:48:49 +0100

    q5

--- q5.py
+++ q5.py
@@ -3,6 +3,19 @@
 import numpy as np
 
 def question05(allowedAllocations, totalValue):
+  nums = []
+  for i in range(0, totalValue+1):
+      nums.append(-1)
+  for n in allowedAllocations:
+      nums[n] = 1
+  for i in range(0, totalValue+1):
+      print(nums)
+      for allocation in allowedAllocations:
+          prevNum = i - allocation
+          if prevNum > 0 and nums[prevNum] > 0:
+              if nums[i] == -1 or nums[i] > nums[prevNum] + 1:
+                  nums[i] = nums[prevNum] + 1
+  return nums[totalValue]
+
   # modify and then return the variable below
-  answer = -1
-  return answer
+
