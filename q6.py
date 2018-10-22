4a0724d0cfb5e632238b063976901f11e9536b7e
mibobkov <mibobkov@gmail.com>
2018-10-22 18:56:55 +0100

    q6

--- q6.py
+++ q6.py
@@ -4,5 +4,13 @@ import numpy as np
 
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
-  answer = -1
-  return answer
+  map = times[0].copy()
+  for i in range(numServers):
+    for j in range(numServers):
+        if j == i:
+          continue
+        if map[i] + times[i][j] < map[j]:
+          map[j] = map[i] + times[i][j]
+  return map[targetServer]
+
+# print(question06(3,1,[[0,7,4],[7,0,2],[4,2,0]]))
