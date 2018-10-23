80c009234cc30898b87496deeec7b27f2faf2eee
mibobkov <mibobkov@gmail.com>
2018-10-23 10:54:18 +0100

    q6 improved

--- q1.py
+++ q1.py
@@ -9,7 +9,7 @@ def question01(portfolios):
   for port in portfolios:
       root.insert(port)
 
-  root.PrintTree('')
+  # root.PrintTree('')
   answer = getMaxValue(root, root)
   return answer
 
@@ -58,7 +58,7 @@ def getMaxValue(node1, node2):
     return val
 
 
-# print('Answer: ' + str(question01([31, 9, 7, 12])))
+print('Answer: ' + str(question01([31, 9, 7, 12])))
 # print('Answer: ' + str(question01([1, 9])))
 # print('Answer: ' + str(question01([2**15, 4])))
 # print('Answer: ' + str(question01([2**16-1, 9,1,4,2,1,6,467,657,45,674,567,456,745,674,5672,354,345,54366,34562,23455,23451,53425, 7, 12])))
--- q6.py
+++ q6.py
@@ -4,13 +4,27 @@
 
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
-  map = times[0][:]
-  for i in range(numServers):
-    for j in range(numServers):
-        if j == i:
-          continue
-        if map[i] + times[i][j] < map[j]:
-          map[j] = map[i] + times[i][j]
-  return map[targetServer]
 
-print(question06(3,1,[[0,7,4],[7,0,2],[4,2,0]]))
+  sofar = times[0][:]
+  reached = [0]
+  for iter in range(numServers-1):
+      # print('Reached: ' + str(reached))
+      for r in reached:
+          for i in range(numServers):
+              if i == r:
+                  continue
+              if times[r][i] + sofar[r] < sofar[i]:
+                  sofar[i] = times[r][i] + sofar[r]
+      minVal = -1
+      for s in range(len(sofar)):
+          if s in reached:
+              continue
+          elif minVal == -1 or minVal > sofar[s]:
+              minVal = sofar[s]
+              minInd = s
+      reached.append(minInd)
+      if targetServer in reached:
+          return sofar[targetServer]
+  return sofar[targetServer]
+
+# print(question06(3,1,[[0,7,4],[7,0,2],[4,2,0]]))
